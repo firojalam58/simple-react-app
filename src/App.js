3a473e05d007260a5565509e705557f3184bdd30
Firoj Alam Raju <firojalamraju85@gmail.com>
2022-10-25 18:33:43 +0600

    header section edited

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
 
 function App() {
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,17 +7,26 @@ const Header = () => {
     return (
             
    <div>
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-           <Link className='link' to={'/'}>Home</Link>
-           <Link className='link' to={'/about'}>About</Link>
-           <Link className='link' to={'/course'}>Course</Link>
-           <Link className='link' to={'/blog'}>Blog</Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <Navbar collapseOnSelect className='mb-4' expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand><Link to='/'><img src={images} alt="" /></Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="m-auto">
+                        <Link className='link' to={'/home'}>Home</Link>
+                        <Link className='link' to={'/about'}>About Us</Link>
+                        <Link className='link' to={'/blog'}>Blog</Link>
+                        <Link className='link' to={'/course'}>Courses</Link>
+                        <Link className='link' to={'/contract'}>Contract</Link>
+                    </Nav>
+                    <Nav>
+                        
+
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
 
 </div>
 
